9feb6391021608cbbd4c851f51ce3ad1c3015577
Eyong Nsoesie <eyong.ns@gmail.com>
2013-09-07 17:56:27 -0400

    First pass and providing a JSON-based API for the server. The mobile clients will communicate with the server via this API

---
+++ src/messenger/webim/mobile/functions.php
@@ -0,0 +1,434 @@
+<?php
+
+require_once('../libs/common.php');
+require_once('../libs/operator.php');
+require_once('../libs/chat.php');
+require_once('../libs/userinfo.php');
+require_once('../libs/groups.php');
+
+/* Mobile client error codes
+	0 = success
+	1 = login failed
+	2 = invalid operator token
+*/
+
+
+// Todo: These two arrays are from operator/upate.php.
+// They need to be put in a common place to be shared rather than duplicated
+$threadstate_to_string = array(
+	$state_queue => "wait",
+	$state_waiting => "prio",
+	$state_chatting => "chat",
+	$state_closed => "closed",
+	$state_loading => "wait",
+	$state_left => "closed"
+);
+
+$threadstate_key = array(
+	$state_queue => "chat.thread.state_wait",
+	$state_waiting => "chat.thread.state_wait_for_another_agent",
+	$state_chatting => "chat.thread.state_chatting_with_agent",
+	$state_closed => "chat.thread.state_closed",
+	$state_loading => "chat.thread.state_loading"
+);
+
+
+function mobile_login($username, $password) {
+	if (isset($username) && isset($password)) {
+		// Note: Blank passwords not currently allowed.
+		$op = operator_by_login($username);
+	
+		if (isset($op) && md5($password) == $op['vcpassword']) {
+			$oprtoken = create_operator_session($op);
+			$out = array('oprtoken' => $oprtoken,
+						 'operatorid' => $op['operatorid'],
+						 'fullname' => $op['vclocalename'],
+						 'status' => $op['istatus'],
+						 'lastvisited' => $op['dtmlastvisited'],
+						 'errorMsg' => 'success',
+						 'errorCode' => 0);
+			return $out;
+		}
+	}
+	
+	$out = array('errorMsg' => 'login failed',
+				 'errorCode' => 1);
+	
+	return $out;
+}
+
+function chat_server_status() {
+	global $jsver;
+	$out = array('version' => $jsver,
+				 'server_status' => 'on',
+				 'errorCode' => 0,
+				 'errorMsg' => 'success');
+				 
+	return $out;
+}
+
+function create_operator_session($op) {
+	global $mysqlprefix;
+	$link = connect();
+	
+	// Return the current unexpired token if the user is already logged in.
+	$loggedInOp = select_one_row("select * from ${mysqlprefix}chatoperatorsession
+								 where operatorid = ". $op['operatorid'], $link);
+	
+	if ($loggedInOp != null) {
+		mysql_close($link);
+		return $loggedInOp['oprtoken'];
+	}
+
+	// Token is the first 10 characters of the md5 of the current time
+	$oprtoken = strtoupper(substr(md5(time()), 0, 10));
+	
+	$query = sprintf("insert into ${mysqlprefix}chatoperatorsession (operatorid, oprtoken) values (%d, '%s')",
+					 $op['operatorid'],
+					 $oprtoken);
+	perform_query($query, $link);
+
+	mysql_close($link);
+	
+	return $oprtoken;
+}
+
+
+/***********
+ * Method:	
+ *		get_active_visitors
+ * Description:
+ *	  	Returns a list of active visitors.
+ *	  	i.e, those that are waiting for an operator as well as
+ *	    those who already have a chat in session
+ * Author:
+ * 		ENsoesie 	9/4/2013	Creation
+ ***********/
+function get_active_visitors($oprtoken) {
+	$operatorId = operator_from_token($oprtoken);
+	
+	if ($operatorId != NULL) {
+		$out = get_pending_threads();
+	}
+	else {
+		$out = array('errorCode' => 2,
+					 'errorMsg' => 'invalid operator token');
+	}
+	
+	return $out;
+}
+
+
+/***********
+ * Method:	
+ *		operator_from_token
+ * Description:
+ *	  	Checks whether the token is valid, i.e, exists and (todo) not expired.
+ *		Returns the associated operator id if it is valid
+ * Author:
+ * 		ENsoesie 	9/4/2013	Creation
+ ***********/
+function operator_from_token($oprtoken) {
+	global $mysqlprefix;
+	$link = connect();
+	
+	$query = "select operatorid from ${mysqlprefix}chatoperatorsession where oprtoken = '$oprtoken'";
+	$row = select_one_row($query, $link);
+	
+	mysql_close($link);
+	
+	return $row['operatorid'];
+}
+
+/***********
+ * Method:	
+ *		get_pending_threads
+ * Description:
+ *	  	This is just like print_pending_threads from operator/update.php, 
+ * 		except that the output is an array, with mobile friendly data
+ * Author:
+ * 		ENsoesie 	9/4/2013	Creation
+ ***********/
+function get_pending_threads()
+{
+	global $webim_encoding, $settings, $state_closed, $state_left, $mysqlprefix;
+	$link = connect();
+
+	$output = array();
+	$query = "select threadid, userName, agentName, unix_timestamp(dtmcreated), userTyping, " .
+			 "unix_timestamp(dtmmodified), lrevision, istate, remote, nextagent, agentId, userid, shownmessageid, userAgent, (select vclocalname from ${mysqlprefix}chatgroup where ${mysqlprefix}chatgroup.groupid = ${mysqlprefix}chatthread.groupid) as groupname " .
+			 "from ${mysqlprefix}chatthread where istate <> $state_closed AND istate <> $state_left " . 
+			 "ORDER BY threadid";
+	$rows = select_multi_assoc($query, $link);
+	
+	foreach ($rows as $row) {
+		$thread = thread_to_array($row, $link);
+		$output[] = $thread;
+	}
+
+	mysql_close($link);
+
+	//foreach ($output as $thr) {
+		//print myiconv($webim_encoding, "utf-8", $thr);
+	//}
+	
+	return $output;
+}
+
+function thread_to_array($thread, $link)
+{
+	global $state_chatting, $threadstate_to_string, $threadstate_key,
+$webim_encoding, $operator, $settings,
+$can_viewthreads, $can_takeover, $mysqlprefix;
+	$state = $threadstate_to_string[$thread['istate']];
+	
+	$result = array();
+	$result['threadid'] = $thread['threadid'];
+	
+	if ($state == "closed") {
+		$result['state'] = $state;
+		return $result;
+	}
+
+	$state = getstring($threadstate_key[$thread['istate']]);
+	$nextagent = $thread['nextagent'] != 0 ? operator_by_id_($thread['nextagent'], $link) : null;
+	$threadoperator = $nextagent ? get_operator_name($nextagent)
+			: ($thread['agentName'] ? $thread['agentName'] : "-");
+
+	if ($threadoperator == "-" && $thread['groupname']) {
+		$threadoperator = "- " . $thread['groupname'] . " -";
+	}
+
+	if (!($thread['istate'] == $state_chatting && $thread['agentId'] != $operator['operatorid'] && !is_capable($can_takeover, $operator))) {
+		$result['canopen'] = "true";
+	}
+	if ($thread['agentId'] != $operator['operatorid'] && $thread['nextagent'] != $operator['operatorid']
+		&& is_capable($can_viewthreads, $operator)) {
+		$result['canview'] = "true";
+	}
+	if ($settings['enableban'] == "1") {
+		$result['canban'] = "true";
+	}
+
+	$banForThread = $settings['enableban'] == "1" ? ban_for_addr_($thread['remote'], $link) : false;
+	if ($banForThread) {
+		$result['ban'] = "blocked";
+		$result['banid'] = $banForThread['banid'];
+	}
+
+	$result['state'] = $state;
+	$result['typing'] = $thread['userTyping'];
+	
+	if ($banForThread) {
+		$name = htmlspecialchars(getstring('chat.client.spam.prefix'));
+	}
+	$name .= htmlspecialchars(htmlspecialchars(get_user_name($thread['userName'], $thread['remote'], $thread['userid'])));
+	
+	$result['name'] = $name;
+	
+//	$result['addr'] = htmlspecialchars(get_user_addr($thread['remote']));
+	$result['agent'] = htmlspecialchars(htmlspecialchars($threadoperator));
+	$result['time'] = $thread['unix_timestamp(dtmcreated)'] . "000";
+	$result['modified'] = $thread['unix_timestamp(dtmmodified)'] . "000";
+
+	if ($banForThread) {
+		$result['reason'] = $banForThread['comment'];
+	}
+
+	$userAgent = get_useragent_version($thread['userAgent']);
+	$result['useragent'] = $userAgent;
+	if ($thread["shownmessageid"] != 0) {
+		$query = "select tmessage from ${mysqlprefix}chatmessage where messageid = " . $thread["shownmessageid"];
+		
+		// I can't explain why using $link causes select_one_row to fail
+		$newlink = connect();
+		$line = select_one_row($query, $newlink);
+		mysql_close($newlink);
+
+		if ($line) {
+			$message = preg_replace("/[\r\n\t]+/", " ", $line["tmessage"]);
+			$result['message'] = htmlspecialchars(htmlspecialchars($message));
+		}
+	}
+
+	return $result;
+}
+
+
+/***********
+ * Method:	
+ *		start_chat
+ * Description:
+ *	  	Start a chat with a visitor. This constitutes a new chat, continuing the chat, 
+ * 		or taking over the chat.
+ * Author:
+ * 		ENsoesie 	9/4/2013	Creation
+ ***********/
+function start_chat($oprtoken, $threadid) {
+	$operatorId = operator_from_token($oprtoken);
+	if ($operatorId == NULL) {
+		return array('errorCode' => 2,
+					 'errorMsg' => 'invalid operator token');
+	}
+
+	$operator = operator_by_id($operatorId);
+	$chattoken = $_GET['token'];
+
+	$thread = thread_by_id($threadid);
+	if (!$thread || !isset($thread['ltoken'])) {
+		return array('errorCode' => 3,
+					 'errorMsg' => 'invalid thread');
+	}
+
+	// If token is not set, this is a new chat session for this operator
+	if (!isset($chattoken)) {
+		$viewonly = filter_var($_GET['viewonly'], FILTER_VALIDATE_BOOLEAN);
+		$forcetake = filter_var($_GET['force'], FILTER_VALIDATE_BOOLEAN);
+
+		if (!$viewonly && $thread['istate'] == $state_chatting && 
+			$operator['operatorid'] != $thread['agentId']) {
+			
+			if (!is_capable($can_takeover, $operator)) {
+				return array('errorCode' => 4,
+							 'errorMsg' => 'cannot take over');
+			}
+			
+			if ($forcetake == false) {
+				// Todo. Confirm that you want to force the takeover of the conversation
+				return array('errorCode' => 5,
+							 'errorMsg' => 'confirm take over');
+			}
+		}
+
+		if (!$viewonly) {
+			take_thread($thread, $operator);
+		} else if (!is_capable($can_viewthreads, $operator)) {
+			return array('errorCode' => 6,
+						 'errorMsg' => 'cannot view thread');
+		}
+		
+		$chattoken = $thread['ltoken'];
+	}
+
+	// Chat token may be different if token was supplied from the http request
+	if ($chattoken != $thread['ltoken']) {
+		return array('errorCode' => 7,
+					 'errorMsg' => 'wrong thread');
+	}
+	
+	if ($thread['agentId'] != $operator['operatorid'] && 
+		!is_capable($can_viewthreads, $operator)) {
+		return array('errorCode' => 6,
+					 'errorMsg' => 'cannot view thread');
+	}
+	
+	$out = array('errorCode' => 0,
+				 'errorMsg' => 'success',
+				 'threadid' => $threadid,
+				 'chattoken' => $chattoken);
+	return $out;
+}
+
+/***********
+ * Method:	
+ *		get_new_messages
+ * Description:
+ *	  	Get messages that have not yet been sync'ed for the current chat session
+ * Author:
+ * 		ENsoesie 	9/7/2013	Creation
+ ***********/
+function get_new_messages($oprtoken, $threadid, $chattoken) {
+	$operatorId = operator_from_token($oprtoken);
+	if ($operatorId == NULL) {
+		return array('errorCode' => 2,
+					 'errorMsg' => 'invalid operator token');
+	}
+
+	$operator = operator_by_id($operatorId);
+	$thread = thread_by_id($threadid);
+	if (!$thread || !isset($thread['ltoken'])) {
+		return array('errorCode' => 3,
+					 'errorMsg' => 'invalid thread');
+	}
+	
+	if ($chattoken != $thread['ltoken']) {
+		return array('errorCode' => 8,
+					 'errorMsg' => 'invalid chat token');
+	}
+	
+	return get_unsynced_messages($threadid);
+}
+
+/***********
+ * Method:	
+ *		get_unsynced_messages
+ * Description:
+ *	  	Helper method to get messages that have not 
+ *		yet been sync'ed for the current chat session
+ * Author:
+ * 		ENsoesie 	9/7/2013	Creation
+ ***********/
+function get_unsynced_messages($threadid) {
+	$link = connect();
+
+	$query = "select messageid, tmessage, dtmcreated from ${mysqlprefix}chatmessage 
+			  where threadid = $threadid";
+	
+	$rows = select_multi_assoc($query, $link);
+
+	mysql_close($link);
+
+	$out = array('errorCode' => 0,
+				 'errorMsg' => 'success');
+				 
+	$out['messageCount'] = count($rows);
+
+	// Make sure there is at least one result
+	if (count($rows) > 0) {
+		$out['messages'] = $rows;
+	}
+	
+	return $out;
+}
+
+
+/***********
+ * Method:	
+ *		msg_from_mobile_op
+ * Description:
+ *	  	Post a message from the mobile operator
+ * Author:
+ * 		ENsoesie 	9/7/2013	Creation
+ ***********/
+function msg_from_mobile_op($oprtoken, $threadid, $chattoken, $opMsg) {
+	$operatorId = operator_from_token($oprtoken);
+	if ($operatorId == NULL) {
+		return array('errorCode' => 2,
+					 'errorMsg' => 'invalid operator token');
+	}
+
+	$operator = operator_by_id($operatorId);
+	$thread = thread_by_id($threadid);
+	if (!$thread || !isset($thread['ltoken'])) {
+		return array('errorCode' => 3,
+					 'errorMsg' => 'invalid thread');
+	}
+	
+	if ($chattoken != $thread['ltoken']) {
+		return array('errorCode' => 8,
+					 'errorMsg' => 'invalid chat token');
+	}
+
+
+	global $kind_agent;
+	$from = $thread['agentName'];
+
+	$link = connect();
+	$postedid = post_message_($threadid, $kind_agent, $opMsg, $link, $from, null, $operatorId);
+	mysql_close($link);
+	
+	return array('errorCode' => 0,
+			     'errorMsg' => 'success');
+}
+
+?>
